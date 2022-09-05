19f8e9f66207edcb9c3ae5851a831da2b687aa89
Ankesh Kumar <ankesh9053@gmail.com>
2022-09-05 08:01:26 +0530

    first commit

--- js_101.js
+++ js_101.js
@@ -5,3 +5,10 @@ name = "Sanjeev kumar";
 console.log(name);
 name  = "Beena devi";
 console.log(name);
+
+var x;
+x = "Masai School"
+console.log(x);
+y = "A Transformation in Education";
+console.log(y);
+
