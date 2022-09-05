bdd7a0be9306dd7624d7dd4b7490fa56d7462ddd
Ankesh Kumar <ankesh9053@gmail.com>
2022-09-05 08:07:38 +0530

    first commit

--- js_101.js
+++ js_101.js
@@ -20,3 +20,37 @@ console.log(typeof(name));
 console.log(typeof(age));
 
 
+report = "******************REPORT_CARD*******************"
+var name = "Name          = Ankesh kumar";
+grade = "Grade         = 12th";
+rollnumber = 21;
+section = "Section       = A";
+rollnumber = "rollno        = 21";
+school = "school name   = Grace public school";
+subject = "subjects name = English   Hindi   Maths"
+marks = "Total Marks   = 100       100     100";
+marks1 ="Obtained Marks= 99        100     100";
+
+console.log("###########################################");
+console.log("###########################################");
+console.log("###########################################");
+console.log("|------------------------------------------|");
+console.log("|------------------------------------------|");
+console.log("|------------------------------------------|");
+console.log("|__________________________________________|");
+console.log("|           ### REPORT CARD ###            |");
+console.log("|__________________________________________|");
+console.log("|                                          |");
+console.log("|------------------------------------------|");
+console.log("|------------------------------------------|");
+console.log("|__________________________________________|");
+console.log("###########################################");
+console.log("###########################################");
+console.log("###########################################");
+//console.log(report);
+console.log(name); console.log(grade); console.log(section);console.log(rollnumber);
+console.log(school);console.log(subject);
+console.log(marks);console.log(marks1);
+
+
+ 
